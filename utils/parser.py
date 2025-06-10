325ba684a8faf1d647e855b1cca140d8e5b4a94b
moha0244 <mohamed-lamine.gning@polymtl.ca>
2025-06-10 13:56:53 -0400

    change_object

--- utils/parser.py
+++ utils/parser.py
@@ -1,7 +1,14 @@
 import csv
 
-def parse_icd_file(filepath):
-    with open(filepath, 'r', encoding='utf-8') as f:
-        reader = csv.reader(f, delimiter=';')
-        next(reader)
-        return [row for row in reader]
+
+#def parse_icd_file(filepath):
+#   with open(filepath, 'r', encoding='utf-8') as f:
+#       reader = csv.reader(f, delimiter=';')
+#       next(reader)
+#       return [row for row in reader]
+
+def parse_icd_file(file_obj):
+    decoded = file_obj.read().decode("utf-8").splitlines()
+    reader = csv.reader(decoded, delimiter=';')
+    next(reader)
+    return list(reader)
