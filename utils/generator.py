90cdd295ba9a828b20ab01df101361d1a59568f0
moha0244 <mohamed-lamine.gning@polymtl.ca>
2025-06-11 15:33:46 -0400

    ajout des commentaires

--- utils/generator.py
+++ utils/generator.py
@@ -1,18 +1,28 @@
 from handlers.handleLevel import handle_level_2, handle_level_3, handle_level_4
 
 def generate_derived_parameters(rows):
-    derived_lines = []
-    mbox_name = ""
+    """
+       Parcourt les lignes du fichier ICD et génère les paramètres dérivés.
+
+       Paramètres :
+       - rows : liste des lignes du fichier ICD
+
+       Retour :
+       - Liste des lignes représentant les lignes IADS dérivées.
+       """
+
+    derived_lines = []  # Liste pour accumuler toutes les lignes dérivées générées
+    mbox_name = ""  # Nom de la boîte de message
     for line in rows:
-        level = line[2].strip()
+        level = line[2].strip() # Le champ de niveau est en position 2 (Level 2 / 3 / 4)
         if level == "2":
-            # nom de la boîte de message qui sera utilisé par tous les lignes subséquents de niveau supérieur
+            # Met à jour le nom de la boîte de message qui sera utilisé pour toutes les lignes suivantes
             mbox_name = handle_level_2(line)
         elif level == "3":
-            # Ajout du paramètre dérivé pour la ligne de niveau 3
+            # Génère les lignes dérivées pour un signal de niveau 3
             derived_lines.extend(handle_level_3(line, mbox_name))
         elif level == "4":
-            # Ajout du paramètre dérivé pour la ligne de niveau 4
+            # Génère les lignes dérivées pour un signal multiplexé de niveau 4
             derived_lines.extend(handle_level_4(line, mbox_name))
     return derived_lines
 
